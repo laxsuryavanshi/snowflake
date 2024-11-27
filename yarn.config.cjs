dc9feda86ca6a8849eded38663f5f7e1723b24a3
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2024-11-27 23:41:49 +0530

    set yarn constraints for typescript to version `~5.6.2` in workspace

---
+++ yarn.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('@yarnpkg/types')} */
+
+const { defineConfig } = require('@yarnpkg/types');
+
+module.exports = defineConfig({
+  async constraints({ Yarn }) {
+    for (const dep of Yarn.dependencies({ ident: 'typescript' })) {
+      dep.update('~5.6.2');
+    }
+  },
+});
