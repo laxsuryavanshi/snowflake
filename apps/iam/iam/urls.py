a9c3c0ef9b91507d60aa2af0190f1e477e60301e
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2024-12-01 04:05:43 +0530

    added `iam` django app as an authorization server

---
+++ apps/iam/iam/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from oauth2_provider import urls as oauth2_urls
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("o/", include(oauth2_urls)),
+]
