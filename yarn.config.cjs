0f6752b10190282b3d9a15c8a103301fb965c438
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2024-12-23 01:42:59 +0530

    updated yarn constraints to enforce consistent dependencies across workspace

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,11 +1,42 @@
+// @ts-check
 /** @type {import('@yarnpkg/types')} */
-
 const { defineConfig } = require('@yarnpkg/types');
 
-module.exports = defineConfig({
-  async constraints({ Yarn }) {
-    for (const dep of Yarn.dependencies({ ident: 'typescript' })) {
-      dep.update('~5.6.2');
+/**
+ * This rule will enforce that a workspace MUST depend on the same version of a dependency as
+ * the one used by the other workspaces.
+ *
+ * @param {Context} context
+ */
+function enforceConsistentDependenciesAcrossTheProject({ Yarn }) {
+  for (const dependency of Yarn.dependencies()) {
+    if (dependency.type === 'peerDependencies') {
+      continue;
     }
+
+    for (const otherDependency of Yarn.dependencies({ ident: dependency.ident })) {
+      if (otherDependency.type === 'peerDependencies') {
+        continue;
+      }
+
+      if (
+        (dependency.type === 'devDependencies' || otherDependency.type === 'devDependencies') &&
+        Yarn.workspace({ ident: otherDependency.ident })
+      ) {
+        continue;
+      }
+
+      dependency.update(otherDependency.range);
+    }
+  }
+}
+
+module.exports = defineConfig({
+  async constraints(context) {
+    enforceConsistentDependenciesAcrossTheProject(context);
   },
 });
+
+/**
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Context} Context
+ */
