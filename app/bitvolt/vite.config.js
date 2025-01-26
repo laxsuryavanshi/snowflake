81c64b8a4b6cb1ff6b975fa88ac6ef771fdd9e72
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2025-01-26 20:43:18 +0530

    feat: configure TypeScript paths and Vite alias for improved module resolution

--- app/bitvolt/vite.config.js
+++ app/bitvolt/vite.config.js
@@ -1,6 +1,10 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 };
