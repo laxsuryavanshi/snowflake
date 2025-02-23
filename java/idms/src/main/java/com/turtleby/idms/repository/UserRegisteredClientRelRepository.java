e3a0bf4e8074ce883f9d4dbcac292f2305a03526
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2025-02-23 18:47:54 +0530

    refactor: remove RegisteredClient API and dependencies

--- java/idms/src/main/java/com/turtleby/idms/repository/UserRegisteredClientRelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.turtleby.idms.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.turtleby.idms.entity.UserRegisteredClientRel;
-
-public interface UserRegisteredClientRelRepository extends CrudRepository<UserRegisteredClientRel, Integer> {
-
-}
