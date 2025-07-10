30d02919dbe1b5a34bc05e32a5a2f5f03690089c
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2025-07-10 14:48:20 +0530

    feat(bitvolt): added context, provider, and hook to manage S3 configuration

--- projects/bitvolt/src/app/layout.tsx
+++ projects/bitvolt/src/app/layout.tsx
@@ -2,6 +2,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { Source_Sans_3 } from 'next/font/google';
 
+import { S3ConfigProvider } from '@/context/s3config';
 import './globals.css';
 import { theme } from './theme';
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className={`${sourceSans.variable} antialiased`}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          {children}
+          <S3ConfigProvider>{children}</S3ConfigProvider>
         </ThemeProvider>
       </body>
     </html>
