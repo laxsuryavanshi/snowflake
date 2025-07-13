be20e27b7b883ca58f75b54a22c2334e52dd8a4d
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2025-07-13 13:16:20 +0530

    feat(github.io): initialized a sample Svelte app with github pages deployment using actions

---
+++ projects/github.io/svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const outputDir = 'dist';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: { adapter: adapter({ pages: outputDir, assets: outputDir }) },
+};
+
+export default config;
