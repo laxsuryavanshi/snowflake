2901d59abe735dedc096069a6a260d1fa51d4aff
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2025-08-09 01:24:09 +0530

    chore(*): yarn constraints for repository and license in manifest

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@yarnpkg/types');
+const fs = require('node:fs');
+const path = require('node:path');
+
+/**
+ * Checks if the workspace is the root workspace.
+ *
+ * @param {Workspace} workspace
+ */
+function isRootWorkspace(workspace) {
+  return workspace.cwd === '.';
+}
 
 /**
  * This rule will enforce that a workspace MUST depend on the same version of a dependency as
@@ -29,13 +40,66 @@ function enforceConsistentDependenciesAcrossTheProject({ Yarn }) {
   }
 }
 
+/**
+ * Sets the repository information for a workspace.
+ *
+ * @param {Workspace} workspace
+ */
+function setWorkspaceRepository(workspace) {
+  const repository = {
+    type: 'git',
+    url: 'git+https://github.com/laxsuryavanshi/snowflake.git',
+    directory: workspace.cwd,
+  };
+
+  workspace.set('repository.type', repository.type);
+  workspace.set('repository.url', repository.url);
+  if (!isRootWorkspace(workspace)) {
+    workspace.set('repository.directory', repository.directory);
+  }
+}
+
+/**
+ * Licenses required for public packages.
+ *
+ * @param {Workspace} workspace
+ */
+function setWorkspaceLicense(workspace) {
+  if (!workspace.cwd.startsWith('packages/')) {
+    return;
+  }
+
+  workspace.set('license', 'MIT');
+
+  if (!fs.existsSync(path.join(path.resolve(__dirname), workspace.cwd, 'LICENSE'))) {
+    workspace.error(`LICENSE file is missing in ${workspace.cwd}`);
+  }
+}
+
+/**
+ * Enforces certain fields to be present in all workspaces.
+ *
+ * @param {Context} context
+ */
+function enforceFieldsOnAllWorkspaces({ Yarn }) {
+  for (const workspace of Yarn.workspaces()) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (!workspace.manifest.private) {
+      setWorkspaceLicense(workspace);
+      setWorkspaceRepository(workspace);
+    }
+  }
+}
+
 module.exports = defineConfig({
   // eslint-disable-next-line @typescript-eslint/require-await
   constraints: async ctx => {
     enforceConsistentDependenciesAcrossTheProject(ctx);
+    enforceFieldsOnAllWorkspaces(ctx);
   },
 });
 
 /**
  * @typedef {import('@yarnpkg/types').Yarn.Constraints.Context} Context
+ * @typedef {import('@yarnpkg/types').Yarn.Constraints.Workspace} Workspace
  */
